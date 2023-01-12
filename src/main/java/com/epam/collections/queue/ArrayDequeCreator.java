77029cdd38cfa383e866040cf83fbd033e231cf9
Maksim_Karnitski <maksim.kornitsky@gmail.com>
2023-01-12 12:27:01 +0100

    Implement

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,29 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        Deque<Integer> firstDeque = new ArrayDeque<>(firstQueue);
+        Deque<Integer> secondDeque = new ArrayDeque<>(secondQueue);
+
+        List<Deque<Integer>> list = new ArrayList<>(Arrays.asList(firstDeque, secondDeque));
+        //Game precondition to get two first cards from each queue
+        for (Deque<Integer> q : list) {
+            result.add(q.poll());
+            result.add(q.poll());
+        }
+
+        //Play with cards
+        while (!firstDeque.isEmpty() && !secondDeque.isEmpty()) {
+            for (Deque<Integer> q : list) {
+                q.offerLast(result.pollLast());
+                result.add(q.poll());
+                result.add(q.poll());
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+
+        while (!queue.isEmpty()) {
+            for (int i = 1; i <= everyDishNumberToEat; i++) {
+                int value = queue.poll();
+                if (i == everyDishNumberToEat) {
+                    result.add(value);
+                } else {
+                    queue.offerLast(value);
+                }
+            }
+        }
+        return result;
     }
 }
